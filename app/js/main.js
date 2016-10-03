223c775d5c59447153980ca733135e818ebc738a
Derp McDerpson <number109creatureguy@hotmail.com>
2016-10-03 11:47:01 +1100

    Changed style of docs

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,31 @@
+var rot_dict = {"builtin": 0, "diy": 0, "io": 0}
 
+function openNav() {
+  $("#side").css("width", "250px");
+  $("#main").css("margin-left", "250px");
+};
+
+function closeNav() {
+  $("#side").css("width", "0");
+  $("#main").css("margin-left", "0");
+}
+
+function display(string) {
+  var temp = "#" + string;
+  if (rot_dict[string] === 0) {
+    rot_dict[string] = 90;
+    $(temp + "click").show(500);
+  } else {
+    rot_dict[string] = 0;
+    $(temp + "click").hide(500);
+  }
+  $(temp + "anim").animate(
+    {rotation: rot_dict[string]},
+    {
+      duration: 500,
+      step: function(now) {
+        $(this).css("transform", "rotate(" + now + "deg)");
+      }
+    }
+  );
+}
